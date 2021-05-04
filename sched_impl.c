547b1b77d509d1ccded36157e0f7525dbc7453ed
Imjuakinhere <60617538+Imjuakinhere@users.noreply.github.com>
2021-05-04 14:42:12 -0500

    done

--- sched_impl.c
+++ sched_impl.c
@@ -1,101 +1,90 @@
-//Joaquin Garcia
-
 #include "scheduler.h"
 #include "sched_impl.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <sched.h>
 #include <assert.h>
+sem_t controlSem; //semaphore to control how many threads are in the queue at a time
+sem_t cpuSem; //To allow 1 thread at a time to use the CPU (acts as mutex);
+sem_t emptySem; //acts exactly opposite to controlSem. makes sure queue is not empty. 
 
-// we have 3 the first one to controll the amount of threads in the queua at the time
-// second one will be like out mutex witch will only allow one thread at a time
-// Last one is like the control but it make sure the queue is empty
-sem_t controlSem;
-sem_t cpuSem;
-sem_t emptySem;
+/*** WORKER THREAD OPS ***/
 
-
-/* Fill in your scheduler implementation code below: */
-//First will implemnet the threat portion of the code
+/* Initialize a thread_info_t */
 static void init_thread_info(thread_info_t *info, sched_queue_t *queue)
 {
-	/*...Code goes here...*/
-	info->queue = queue->list;
-    info->queueData = NULL;
+        info->queue = queue->list;
+        info->queueData = NULL;
 }
 
+/* Release the resources associated with a thread_info_t */
 static void destroy_thread_info(thread_info_t *info)
 {
-	/*...Code goes here...*/
-	 free(info->queueData);
+        free(info->queueData);
 }
 
-/*...More functions go here...*/
-
-// This code will block the thread until it can enter the schedualer queue
+/* Block until the thread can enter the scheduler queue. */
 static void enter_sched_queue(thread_info_t *info)
 {
         sem_wait(&controlSem);
         info->queueData = (list_elem_t*)malloc(sizeof(list_elem_t));
         list_elem_init(info->queueData, (void*)info);
         list_insert_tail(info->queue, info->queueData);
-		//list was previously empty notify wait_for_queue
-        if(list_size(info->queue) == 1)
-		{
+        if(list_size(info->queue) == 1)//list was previously empty notify wait_for_queue
                 sem_post(&emptySem);
-		}
         sem_init(&info->runWorker,0,0);
 }
-//removes the thread from the scheduler queue
+
+/* Remove the thread from the scheduler queue. */
 static void leave_sched_queue(thread_info_t *info)
 {
         list_remove_elem(info->queue, info->queueData);
         sem_post(&controlSem);
 }
-// when it's in the process of the scheduler queue it will block until the thread is in the schedual
+
+/* While on the scheduler queue, block until thread is scheduled. */
 static void wait_for_cpu(thread_info_t *info)
 {
         sem_wait(&info->runWorker);
 }
-//it will stop using the CPU when the thread timeslice is over
+
+/* Voluntarily relinquish the CPU when this thread's timeslice is
+ * over (cooperative multithreading). */
 static void release_cpu(thread_info_t *info)
 {
         sem_post(&cpuSem);
         sched_yield();
 }
-//Know we will be worrking on the Sched part of code
+
+/*** SCHED_OPS ***/
+
+/* Initialize a sched_queue_t */
 static void init_sched_queue(sched_queue_t *queue, int queue_size)
 {
-	/*...Code goes here...*/
-	if (queue_size <= 0) 
-	{
-                exit(-1);
-    }
+        if (queue_size <= 0) {
+                exit(-1); //exit entire program if queue has a size of zero
+        }
         queue->currentWorker = NULL;
         queue->nextWorker = NULL;
         queue->list = (list_t*) malloc(sizeof(list_t));
         list_init(queue->list);
         sem_init(&controlSem, 0, queue_size);
-        sem_init(&cpuSem,0,0);
-        sem_init(&emptySem,0,0);
-
+        sem_init(&cpuSem,0,0);//block on first call of wait_for_worker
+        sem_init(&emptySem,0,0);//block on first call of wait_for_queue
 }
-//release anything in the sched_queue_t
+
+/* Release the resources associated with a sched_queue_t */
 static void destroy_sched_queue(sched_queue_t *queue)
 {
-	/*...Code goes here...*/
-	 list_elem_t * temp;
-        while ((temp = list_get_head(queue->list)) != NULL) 
-		{
+        list_elem_t * temp;
+        while ((temp = list_get_head(queue->list)) != NULL) {//delete any remainign list elements
                 list_remove_elem(queue->list, temp);
                 free(temp);
         }
         free(queue->list);
-
 }
 
-/*...More functions go here...*/
-// allow the thread to execute
+/* Allow a worker thread to execute. */
 static void wake_up_worker(thread_info_t *info)
 {
         sem_post(&info->runWorker);
@@ -148,12 +137,9 @@ static void wait_for_queue(sched_queue_t *queue)
         sem_wait(&emptySem);
 }
 
-
-
-/* You need to statically initialize these structures: */
 sched_impl_t sched_fifo = {
-	{ init_thread_info, destroy_thread_info /*, ...etc... */ }, 
-	{ init_sched_queue, destroy_sched_queue /*, ...etc... */ } },
+        { init_thread_info, destroy_thread_info, enter_sched_queue, leave_sched_queue, wait_for_cpu, release_cpu}, 
+        { init_sched_queue, destroy_sched_queue, wake_up_worker, wait_for_worker, next_worker_fifo, wait_for_queue} },
 sched_rr = {
-	{ init_thread_info, destroy_thread_info /*, ...etc... */ }, 
-	{ init_sched_queue, destroy_sched_queue /*, ...etc... */ } };
+        { init_thread_info, destroy_thread_info, enter_sched_queue, leave_sched_queue, wait_for_cpu, release_cpu}, 
+        { init_sched_queue, destroy_sched_queue, wake_up_worker, wait_for_worker, next_worker_rr, wait_for_queue} };
